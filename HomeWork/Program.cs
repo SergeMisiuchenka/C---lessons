2ed4f9d7ba5d4a9e6be2fa9f576f050abb113771
Serge159 <sergemisiuchenka@gmail.com>
2022-05-10 00:20:50 +0300

    5 lesson

---
+++ HomeWork/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main()
+
+        // 2 Урок
+
+        {
+            //    decimal pi = 3.141592653m;
+            //    decimal e = 2.7182818284590452m;
+
+            //    Console.WriteLine(pi);
+
+            //    Console.WriteLine(e);
+
+            //    Console.ReadKey();
+
+            //}
+
+            // .......................3 Урок
+
+            // 2 Задание
+
+            //{
+            //    string n = "Моя строка 1";
+            //    string t = "Моя стрка 2";
+            //    string a = "Моя строка 3";
+
+            //    Console.WriteLine(n);
+            //    Console.WriteLine(t);
+            //    Console.WriteLine(a);
+            //}
+
+            // 3 Задание
+
+            //{
+            //    int x = 10;
+            //    int y = 12;
+            //    int z = 3;
+            //    double result = (x + y + z) / 3f;
+
+
+
+            //    Console.WriteLine(result);
+
+            //}
+
+            // 4 Задание
+
+            //float r = 12f;
+            //float h = 3f;
+            //const float pi = 3.141592653f;
+
+            //double volume = pi * r * r * h;
+            //double s = 2 * pi * r;
+
+            //Console.WriteLine("Объем цилиндра равен " + volume);
+            //Console.WriteLine("Полощадь поверхности цилиндра равна " + s);
+
+
+
+            // .......................4 Урок
+
+            // 1 Задание
+
+
+            //double result = 0;
+
+            //Console.WriteLine("Введите первое число");
+            //string a = Console.ReadLine();
+            //double operand1 = double.Parse(a);
+
+
+            //Console.WriteLine("Введите второе число");
+            //string b = Console.ReadLine();
+            //double operand2 = double.Parse(b);
+
+            //do
+            //{
+            //    Console.WriteLine("Введите второе число");
+            //} while (operand2 >= 0 && operand2 <= 9);
+
+            //switch (b)
+            //{
+            //    case "1":
+            //        {
+            //            break;
+            //        }
+            //    case "2":
+            //        {
+            //            break;
+            //        }
+            //    case "3":
+            //        {
+            //            break;
+            //        }
+            //    case "4":
+            //        {
+            //            break;
+            //        }
+            //    case "5":
+            //        {
+            //            break;
+            //        }
+            //    case "6":
+            //        {
+            //            break;
+            //        }
+            //    case "7":
+            //        {
+            //            break;
+            //        }
+            //    case "8":
+            //        {
+            //            break;
+            //        }
+            //    case "9":
+            //        {
+            //            break;
+            //        }
+            //    case "0":
+            //        {
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            Console.WriteLine("Введите число!");
+            //            break;
+            //        }
+            //}
+
+
+
+            //Console.WriteLine("Введите знак операции");
+            //string sing = Console.ReadLine();
+
+            //switch (sing)
+            //{
+            //    case "+":
+            //        {
+            //            result = operand1 + operand2;
+            //            break;
+            //        }
+            //    case "-":
+            //        {
+            //            result = operand1 - operand2;
+            //            break;
+            //        }
+            //    case "/":
+            //        {
+            //            if (operand1 == 0 || operand2 == 0)
+            //            {
+            //                Console.WriteLine("На ноль делить нельзя!");
+            //            }
+            //            else
+            //            {
+            //                result = operand1 / operand2;
+            //            }
+
+            //            break;
+            //        }
+            //    case "*":
+            //        {
+            //            result = operand1 * operand2;
+            //            break;
+            //        }
+            //    default:
+            //        {
+            //            Console.WriteLine("Вы ввели не знак арифмитической операции!");
+            //            break;
+            //        }
+
+            //}
+            //Console.WriteLine(result);
+            //Console.ReadKey();
+
+
+            // 2 Задание
+
+            //    Console.WriteLine("Введите число");
+            //    string a = Console.ReadLine();
+
+            //    int operand1 = int.Parse(a);
+
+            //if (operand1>=0 && operand1<=14)
+            //{
+            //    Console.WriteLine("Число входит в первый промежуток");
+            //} else if (operand1>=15 && operand1<=35)
+            //{
+            //    Console.WriteLine("Число входит во второй промежуток");
+            //} else if (operand1>=36 && operand1<=49)
+            //{
+            //    Console.WriteLine("Число входит в 3 промежуток");
+            //} else if (operand1 == 50)
+            //{
+            //    Console.WriteLine("Число 50 входит в 3 и в 4 промежуток");
+            //} else if (operand1>=51 && operand1<=100)
+            //{
+            //    Console.WriteLine("Число входит в 4 промежуток");
+            //} else if (operand1<0 || operand1>100)
+            //{
+            //    Console.WriteLine("Число не входит ни в одиз из возможных промежутков!");
+            //}
+            //Console.ReadKey();
+
+
+<<<<<<< HEAD
+
+            // .............................................................5 Урок
+
+            //  1 Задание
+
+            //Console.WriteLine("Enter number");
+            //string operand = Console.ReadLine();
+            //double number = double.Parse(operand);
+
+            //if (number == 0)
+            //{
+            //    Console.WriteLine("Enter another number ");
+            //}
+
+            //else if (number % 2 == 0)
+            //{
+            //    Console.WriteLine("Its even number!");
+            //}
+            //else Console.WriteLine("Its odd number!");
+
+
+            //Console.ReadKey();
+
+
+            // 2 Задание
+
+            //int salary = 500;
+            //Console.WriteLine("Enter duration of work experience");
+            //string a = Console.ReadLine();
+            //double years = double.Parse(a);
+
+            //if (years < 5)
+            //{ Console.WriteLine($"Премия составляет 10% от заработной платы и равна {salary * 10 / 100} "); };
+
+            //if (years > 5 && years <=10)
+            //{ Console.WriteLine($"Премия составляет 15% от заработной платы и равна {salary * 15 / 100} "); }
+
+            //if (years >10 && years <=15)
+            //{ Console.WriteLine($"Премия составляет 25% от заработной платы и равна {salary * 25 / 100} "); }
+
+            //if (years > 15 && years <= 20)
+            //{ Console.WriteLine($"Премия составляет 35% от заработной платы и равна {salary * 35 / 100} "); }
+
+            //if (years > 20 && years <= 25)
+            //{ Console.WriteLine($"Премия составляет 45% от заработной платы и равна {salary * 45 / 100} "); }
+
+            //if (years > 25)
+            //{ Console.WriteLine($"Премия составляет 50% от заработной платы и равна {salary * 50 / 100} "); }
+=======
+            //new line
+
+            //another line
+>>>>>>> c92110f1259f6077150e455e576341f8f9891096
+
+        }
+    }
+}
+
